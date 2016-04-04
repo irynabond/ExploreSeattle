1869bdfd926937a00b4394d4d694044ad106480c
Iryna Bondarenko <irusiabondarenko@gmail.com>
2016-04-03 22:00:15 -0700

    added header to event container

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
  $('#show-events-btn').on('click', function(){
  	var text = $('#searching').val();
  	$('ul').empty();
+ 	$('#header').append('<span class = "event-header">Here you go! Check out these events in Seattle. </span>')
     callApi(text);
    });
  
