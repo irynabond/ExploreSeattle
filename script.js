043c3e44aa4ef00f3580b5640e6392626a6bd560
Iryna Bondarenko <irusiabondarenko@gmail.com>
2016-04-03 23:12:58 -0700

    Added loading animation

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
  $('#show-events-btn').on('click', function(){
+ 	$("#loading").show();
  	var text = $('#searching').val();
  	$("#header").empty();
  	$('ul').empty();
  	callApi(text);
  	setTimeout(function(){
-    $('#header').append('<span class = "event-header">Here you go! Check out these events in Seattle. </span>')
+    $('#header').append('<span class = "event-header">Here you go! Check out these events in Seattle. </span>');
+    $("#loading").hide();
  		$('html, body').animate({
    		 scrollTop: $(".events-content").offset().top
 			}, 1000);
- 	 }, 2000);
+ 	 }, 2500);
+   
  });
  
