3156a6dc5978ef246ae59dbe119e4c6111463755
Iryna Bondarenko <irusiabondarenko@gmail.com>
2016-04-03 22:42:15 -0700

    added scrolling to events

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
  $('#show-events-btn').on('click', function(){
  	var text = $('#searching').val();
+ 	$("#header").empty();
  	$('ul').empty();
- 	$('#header').append('<span class = "event-header">Here you go! Check out these events in Seattle. </span>')
-    callApi(text);
-   });
+ 	callApi(text);
+ 	setTimeout(function(){
+    $('#header').append('<span class = "event-header">Here you go! Check out these events in Seattle. </span>')
+ 		$('html, body').animate({
+   		 scrollTop: $(".events-content").offset().top
+			}, 1000);
+ 	 }, 2000);
+ });
  
