ea8ee442f21981a53e4ab75891ea40dfd1e05b7a
Tina Sharma <tina_2212@hotmail.com>
2016-04-04 17:19:14 -0700

    onkeypress return key working

--- script.js
+++ script.js
@@ -14,3 +14,14 @@
 
  });
 
+function searchKeyPress(e)
+{
+    // look for window.event in case event isn't passed in
+    e = e || window.event;
+    if (e.keyCode == 13)
+    {
+        document.getElementById('show-events-btn').click();
+        return false;
+    }
+    return true;
+}
